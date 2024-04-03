249979aafa3c5e5f31ced4df300f6f7e228fe287
Lex <info@alexandranel.com>
2024-04-03 21:23:27 +1100

    have the login and signup forms up and running, as well as landing page. login and sign up buttons link to pages on the front end. home page / route is written as is login and sign up page. main.handlebars is done also

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 // CRUD
 const router = require('express').Router();
-const {Blog, User} = require ('../models');
+const { Blog, User } = require('../models');
 const withAuths = require('../Utils/auth');
 
 //homepage route homepage will redirect to logged in or not logged in 
@@ -9,6 +9,26 @@ router.get('/', async (req, res) => {
         res.render('homepage', { //express looks for homepage within handlebars
             // passes loggedin state to the template,
             loggedIn: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//login page route 
+router.get('/login', async (req, res) => {
+    try {
+        res.render('login', { //express looks for homepage within handlebars
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//signin page route 
+router.get('/signup', async (req, res) => {
+    try {
+        res.render('signup', { //express looks for homepage within handlebars
         });
     } catch (err) {
         res.status(500).json(err);
---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+//get elements from html
+const loginBtn = document.getElementById("loginButton");
+const logoutBtn = document.getElementById("logoutButton");
+
+
+//event listeners
+
+loginBtn.addEventListener("click", logOut());
+logoutBtn.addEventListener("click", logIn());
+
+//Login Function
+const logIn = async (event) => {
+    event.preventDefault();
+
+    const email = document.query
+
+
+}
