147a173bd2d3b19e217052542209e543509a61a7
Lex <info@alexandranel.com>
2024-04-04 21:46:28 +1100

    have brought users javascript across for login validations

--- public/js/index.js
+++ public/js/index.js
@@ -6,13 +6,59 @@ const logoutBtn = document.getElementById("logoutButton");
 //event listeners
 
 loginBtn.addEventListener("click", logOut());
-logoutBtn.addEventListener("click", logIn());
+logoutBtn.addEventListener("submit", logIn());
+
+// document
+// .querySelector('.login-form')
+// .addEventListener('submit', loginFormHandler);
+
+// document
+// .querySelector('.signup-form')
+// .addEventListener('submit', signupFormHandler);
 
 //Login Function
 const logIn = async (event) => {
     event.preventDefault();
+  
+    const email = document.querySelector('#userEmail').value.trim();
+    const password = document.querySelector('#loginPassword').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in. Have you signed up?');
+      }
+    }
+  };
+  
+  const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to sign up.');
+      }
+    }
+  };
+  
 
-    const email = document.query
-
-
-}
+  
