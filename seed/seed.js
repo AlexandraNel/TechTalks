c1a2facbedd137d9305ac891ea64b4471d5ad8a8
Lex <info@alexandranel.com>
2024-05-05 19:51:20 +1000

    heroku will not deploy

--- seed/seed.js
+++ seed/seed.js
@@ -1,30 +1,25 @@
 const sequelize = require('../config/connection');
-const { User, Blog} = require('../models');
+const { User, Blog, } = require('../models');
 
 const userData = require('./userData.json');
 const blogData = require('./blogData.json');
 // const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: false });  // Resets your database, be cautious with this in production
+  await sequelize.sync({ force: true });
 
-  // Bulk create users
   const users = await User.bulkCreate(userData, {
-      individualHooks: true,
-      returning: true,
+    individualHooks: true,
+    returning: true,
   });
 
-  // Bulk create blogs and directly await their creation without storing them
-  await Promise.all(
-      blogData.map(blog => {
-          return Blog.create({
-              ...blog,
-              user_id: users[Math.floor(Math.random() * users.length)].id,  // Randomly assign user ids to blogs
-          });
-      })
-  );
+  for (const blog of blogData) {
+    await Blog.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
 
-  console.log('All models were synchronized successfully.');
   process.exit(0);
 };
 
