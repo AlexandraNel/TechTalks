c6c6aa7e4ff0b700e572a97a8c31131bfb6851ba
Lex <info@alexandranel.com>
2024-05-05 17:23:11 +1000

    have fixed up comments and updated layouts

--- public/js/comment.js
+++ public/js/comment.js
@@ -35,32 +35,34 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Event listener for clicking the "Delete" button for a comment
 document.addEventListener('DOMContentLoaded', function () {
-    const deleteButtons = document.querySelectorAll("delete-comment");
-    if (deleteButtons) {
-        deleteButtons.forEach(button => {
-            button.addEventListener("click", async () => {
-                if (confirm("Are you sure you want to delete this comment?")) {
-                    const commentId = button.dataset.commentId; // Get the comment ID from the button's data attribute
-                    try {
-                        // Send a request to the server to delete the comment
-                        const response = await fetch(`/api/comment/${commentId}`, {
-                            method: 'DELETE'
-                        });
 
-                        if (response.ok) {
-                            // If the request is successful, reload the page to show the updated list of comments
-                            document.location.reload();
-                        } else {
-                            // If the request fails, log an error message
-                            console.error('Failed to delete comment');
-                        }
-                    } catch (err) {
-                        console.error(err); // Log any errors that occur during the fetch request
+    const deleteButtons = document.querySelectorAll(".delete-comment");
+
+
+    deleteButtons.forEach(button => {
+
+        button.addEventListener("click", async () => {
+
+            if (confirm("Are you sure you want to delete this comment?")) {
+                const commentId = button.dataset.commentId; // Get the comment ID from the button's data attribute
+                try {
+                    // Send a request to the server to delete the comment
+                    const response = await fetch(`/api/comments/${commentId}`, {
+                        method: 'DELETE'
+                    });
+
+                    if (response.ok) {
+                        // If the request is successful, reload the page to show the updated list of comments
+                        document.location.reload();
+                    } else {
+                        // If the request fails, log an error message
+                        console.error('Failed to delete comment');
                     }
+                } catch (err) {
+                    console.error(err); // Log any errors that occur during the fetch request
                 }
-            });
+            }
         });
-    } else {
-        console.error("Error with the delete comment button");
-    }
+    });
+
 });
