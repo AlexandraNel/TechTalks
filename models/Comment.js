75b64eb4bf401c4a7323ef759057362a24f73062
Lex <info@alexandranel.com>
2024-05-06 18:26:41 +1000

    Updated User capitalisation

--- models/Comment.js
+++ models/Comment.js
@@ -26,7 +26,7 @@ Comment.init(
         user_id: {
             type: DataTypes.INTEGER,
             references: {
-                model: 'user',
+                model: 'User',
                 key: 'id',
             }
         },
